9cc339cce09a2ae3eb8062254c59fbd66552e30a
Ajay Maida <ajaymaida98@gmail.com>
2022-12-19 16:30:20 +0530

    short_cut for cpp

--- project/mergeJoin.cpp
+++ project/mergeJoin.cpp
@@ -15,6 +15,7 @@ int ALL_cost = 0;
 string path_to_store = "C:\\Users\\ajaym\\Documents\\new_visual\\project";
 // #define path_to_store "C:\\Users\\ajaym\\Documents\\new_visual\\project"
 
+// comparator
 class cmp
 {
 public:
@@ -23,14 +24,15 @@ public:
         return v1[0] > v2[0];
     }
 };
+
 class Relation
 {
     int TB = 10; // number of tupple per block
     int MB;      // total mem block
-    int b;       // total number of clock in relation
+    int b;       // total number of block in relation
     int n;       // total number of tupple in relation
     int dup;
-    int cost = 0;
+    int cost = 0; //
     string tab_file_path;
     ifstream tab_file;
     string table_name;
@@ -38,23 +40,21 @@ class Relation
     string table_nam;
     int block_to_re_fro_sor_run; // how many block do we need to read from each sorted_runs(as per the mem block cap)
     vps file_vec;                // temp vector which will store block(some) of relation while reading from file
-    vs sorted_runs_path;
+    vs sorted_runs_path;         // store the sorted runs file's path
     vf sorted_runs_file;
     // vi frez_count;
 
 public:
-   
-
     void dis()
     {
         cout << "Info about table :\n";
-        cout<<table_nam<<"\n";
-        cout<<col_info<<"\n";
+        cout << table_nam << "\n";
+        cout << col_info << "\n";
         cout << "Total number of Blocks : " << b << "\n";
         cout << "Total number of Tupples : " << n << "\n";
         cout << "Total sorted runs created : " << sorted_runs_path.size() << "\n";
         cout << "Total cost of sorting in term of disc block reads : " << cost << "\n";
-        ALL_cost+=cost;
+        ALL_cost += cost;
     }
     Relation(string path, int mb, string tab_nam)
     {
@@ -70,6 +70,7 @@ public:
         int idx = 0;
         string line, key;
 
+        // runs till the end of table
         while (getline(tab_file, line))
         {
             if (tab_file.good() && !line.empty())
@@ -92,12 +93,13 @@ public:
                 count++;
                 if (file_vec.size() == MB * TB)
                 {
+                    // sorting the tuple inside the vector
                     sort(file_vec.begin(), file_vec.end());
                     string pat = path_to_store + "\\" + table_name + "\\" + "_" + to_string(idx) + ".txt";
                     sorted_runs_path.push_back(pat);
                     wri_buf(file_vec, pat);
                     idx++;
-                    file_vec.clear();
+                    file_vec.clear(); // flush
                 }
             }
         }
@@ -113,7 +115,7 @@ public:
         n = (idx - 1) * TB * MB + file_vec.size();
         file_vec.clear();
         b = ceil((float)n / (float)TB);
-        cost += b;
+        cost += b * 2;
     }
 
     void wri_buf(vector<pair<string, string>> &vec, string path)
@@ -141,12 +143,12 @@ public:
         while (i < sorted_runs_path.size())
         {
             sorted_runs_file[i]->close();
-            // free(sorted_runs_file[i]);
+            free(sorted_runs_file[i]); // deallocate mem
             i++;
         }
     }
 
-    bool full_container(int sorted_file_num, vvvs &container_queue, vector<ifstream *> const &sorted_files, int B)
+    bool full_container(int sorted_file_num, vvvs &container_vec, vector<ifstream *> const &sorted_files, int B)
     {
         string line;
         int i = 1;
@@ -156,22 +158,37 @@ public:
             flag = true;
             int offset = line.find_first_of(",");
             string key = line.substr(1, offset - 1);
-            container_queue[sorted_file_num].push_back({key, to_string(sorted_file_num), line});
+            container_vec[sorted_file_num].push_back({key, to_string(sorted_file_num), line});
             i++;
         }
-        cost += ceil((float)(i - 1) / (float)TB);
+        cost += (ceil((float)(i - 1) / (float)TB)) * 2; // multiply by 2 because we have to write back also
+
         return flag;
     }
     string merge_sorted_runs()
     {
         string final_sorted_file_path;
-        int block_to_read_from_sorted_run = floor(((float)(MB) / (float)sorted_runs_path.size()));
+        // reserving some block for writing the update value ( for min heap)
+        int block_to_read_from_sorted_run = floor(((float)(MB - ceil((float)sorted_runs_file.size() / (float)TB)) / (float)sorted_runs_path.size()));
 
-        vvvs container_queue(sorted_runs_path.size());
-        priority_queue<vs, vvs, cmp> final_sorted_que;
+        vvvs container_vec(sorted_runs_path.size());
+        priority_queue<vs, vvs, cmp> final_sorted_que; // min heap
+        // used to open files of sorted runs
         open_sorted_runs_file(sorted_runs_path, sorted_runs_file);
         //
-        int i = 0;
+        int i = 0; // specify sorted runs num
+        // if we have only one sorted run ,we don't have to do anything just give same sorted run as final sorted file
+        // check memory constraint
+        if (sorted_runs_file.size() >= MB)
+        {
+            cout << "Memory constraint violeted !";
+            exit(-1);
+        }
+        if (sorted_runs_path.size() == 1)
+        {
+            final_sorted_file_path = sorted_runs_path[0];
+            return final_sorted_file_path;
+        }
         while (i < sorted_runs_path.size())
         {
             int j = 1;
@@ -181,20 +198,22 @@ public:
                 int offset = line.find_first_of(",");
                 string key = line.substr(1, offset - 1);
                 // key,num_sorted_run,line
-                container_queue[i].push_back({key, to_string(i), line});
+                container_vec[i].push_back({key, to_string(i), line});
                 // cout << key << "\n";
                 j++;
             }
-            if (!container_queue[i].empty())
+            // adding block read into cost when we are filling buffer coresponding to each run file at the beginning
+            cost += ceil((j - 1) / TB) * 2; // multiply by 2 b/c we write back
+            if (!container_vec[i].empty())
             {
 
-                container_queue[i].erase(container_queue[i].begin());
-                final_sorted_que.push({container_queue[i][0]});
+                // picking the first tuple from the sorted run and pushing into min heap
+                final_sorted_que.push({container_vec[i][0]});
+                // erasing the same tuple from the corresponding buffer of sorted run
+                container_vec[i].erase(container_vec[i].begin());
             }
             i++;
         }
-        // adding block read into cost when we are filling buffer coresponding to each run file at the beginning
-        cost += block_to_read_from_sorted_run * sorted_runs_path.size();
 
         final_sorted_file_path = path_to_store + "\\" + table_name + "\\" + "sorted_Table_" + table_name + ".txt";
         ofstream final_sorted_file(final_sorted_file_path);
@@ -204,21 +223,22 @@ public:
             final_sorted_que.pop();
             final_sorted_file << temp_vec[2];
             final_sorted_file << "\n";
-            if (!container_queue[stoi(temp_vec[1])].empty() || full_container(stoi(temp_vec[1]), container_queue, sorted_runs_file, block_to_read_from_sorted_run))
+            if (!container_vec[stoi(temp_vec[1])].empty() || full_container(stoi(temp_vec[1]), container_vec, sorted_runs_file, block_to_read_from_sorted_run))
             {
-                final_sorted_que.push({container_queue[stoi(temp_vec[1])][0]});
-                container_queue[stoi(temp_vec[1])].erase(container_queue[stoi(temp_vec[1])].begin());
+                final_sorted_que.push({container_vec[stoi(temp_vec[1])][0]});
+                container_vec[stoi(temp_vec[1])].erase(container_vec[stoi(temp_vec[1])].begin());
             }
         }
         close_sorted_runs_file();
         return final_sorted_file_path;
     }
 };
-class MergeJoin
+
+class sortMergeJoin
 {
     int const TB = 10;
     int MB;
-    int cost=0;
+    int cost = 0;
     int b = 0; // total number of block in merged table
     int n = 0; // total number of tupple in merge talble
     int tab1_st = 0;
@@ -234,22 +254,24 @@ class MergeJoin
     vps tab1_buff, tab2_buff;
 
 public:
-int Cost(){return cost;}
+    int Cost() { return cost; }
     void dis()
     {
         cout << "\n\nMerge Table Info : \n";
         cout << "total number of block : " << b;
         cout << "\ntotal number ot tupple : " << n << "\n";
-        cout<<"Total cost of join : "<<cost<<"\n";
-        ALL_cost+=cost;
-        cout <<"Joined Table Path : "<< merge_table_path << "\n\n";
+        cout << "Total cost of join : " << cost << "\n";
+        cout << "Joined Table Path : " << merge_table_path << "\n\n";
+        ALL_cost += cost;
     }
-    MergeJoin(string path1, string path2, int m)
+    sortMergeJoin(string path1, string path2, int m)
     {
         MB = m;
-        block_of_each_rel = (MB - 1) / 2;
+
+        block_of_each_rel = (MB - 1) / 2; // 1 block reserved for output buffer
         tab1_file.open(path1);
         tab2_file.open(path2);
+        // finally merged table path
         merge_table_path = path_to_store + "\\" + "merged_tab" + ".txt";
         fin_merg_tab.open(path_to_store + "\\" + "merged_tab" + ".txt");
         // cout << "\ntable 1 is being filled\n";
@@ -272,7 +294,7 @@ int Cost(){return cost;}
             buff.push_back({key, line});
             i++;
         }
-        cost+=ceil((float)(i-1)/(float)TB);
+        cost += (ceil((float)(i - 1) / (float)TB)) * 2;
         // cout<<tab.eof()<<"\n";
         // cout << "block size : " << buff.size() / TB << "\n";
         // cout << "buffer size :" << buff.size() << "\n";
@@ -288,7 +310,8 @@ int Cost(){return cost;}
             //             // if(tab1_buff[tab1_st]>tab2_buff[st]){
             //     break;//break b/c there won't be same value on common attribute
             // }
-            while (!tab2_file.eof() && tab2_buff.size() != 1 && en < tab2_buff.size() && tab2_buff[st].fi == tab2_buff[en + 1].fi)
+            // creating group of duplicates having the same join attribute value
+            while (/*!tab2_file.eof() && */ tab2_buff.size() != 1 && en < tab2_buff.size() && tab2_buff[st].fi == tab2_buff[en + 1].fi)
             {
                 // cout << "line 244\n";
                 en++;
@@ -298,7 +321,7 @@ int Cost(){return cost;}
                 //                 if (tab1_buff[tab1_st].fi == tab2_buff[st].fi)
                 {
                     string line = tab1_buff[tab1_st].se;
-                    // remove last ")"
+
                     for (int i = st; i <= en; i++)
                     {
                         fin_merg_tab << line.substr(0, line.size() - 2);
@@ -309,6 +332,7 @@ int Cost(){return cost;}
                         fin_merg_tab << "\n";
                         k++;
                     }
+                    // inctreament the table 1 pointer
                     tab1_st++;
                 }
                 if (tab1_buff.size() == tab1_st)
@@ -318,25 +342,31 @@ int Cost(){return cost;}
                     fill_buff(&tab1_file, tab1_buff);
                 }
             }
+            // increamenting the second table pointer
             tab2_st = en + 1;
             if (tab2_buff.size() == tab2_st)
             {
                 tab2_st = 0;
                 // cout << "\ntable 2 is being filled\n";
+                // just to check if some duplicate of previous group exist in next block we read from disc block
+                tab1_st--;
                 fill_buff(&tab2_file, tab2_buff);
             }
         }
+        // calculating the tuple in finally mergedjoined table
         n = k;
+        // block calculate
         b = ceil((float)k / (float)TB);
     }
 };
 string Sort_the_table(Relation *table)
 {
-    // cout << "\tphaseOne of External sort Merge\n";
+    // phaseOne of External sort Merge
     table->create_sorted_run();
-    // cout << "\tphaseTwo of External sort Merge\n";
+    // phaseTwo of External sort Merge
+    string temp = table->merge_sorted_runs();
     table->dis();
-    return table->merge_sorted_runs();
+    return temp;
 }
 int main(int argc, char *argv[])
 {
@@ -347,15 +377,18 @@ int main(int argc, char *argv[])
     }
 
     string R_path, S_path;
+    // first input first file path
     R_path = argv[1];
+    // second input second file path
     S_path = argv[2];
-    int M = stoi(argv[3]); // memory block available
+    // third input  memory block available
+    int M = stoi(argv[3]);
 
     ifstream fd1(R_path);
     ifstream fd2(S_path);
 
     // check if this file path is valid
-    if (!fd1.good() || !fd2.good() || M <= 0)
+    if (!fd1.good() && !fd2.good() && M <= 0)
     {
         cerr << "Invalid input arguments/n";
         cerr << "Usage : " << argv[0] << " <R_path> <S_path> <sort/hash> <M>/n";
@@ -367,29 +400,30 @@ int main(int argc, char *argv[])
     fd2.close();
 
     // creating object of Relation class for R_table
-
+    // R_table is pointer pointing to the Relation class object having all info regarding the r table
     Relation *R_table = new Relation(R_path, M, "R");
 
     Relation *S_table = new Relation(S_path, M, "S");
     // Relation S_table(S_path, M, "S");
 
-    string final_r, final_s;
     // sort the table and return the final sorted table path
-    cout<<"\n\n\n=================================================================================================================================================\n\n";
-    cout << "Assumption :\n";
-    cout<<"1. Total Main Memory available in term of Frames is : "<<M<<"\n";
-    cout<<"2. Each disc Block can fit into Frame of MM\n\n";
-    cout << "";
-    cout << "\tRelation R sorting started :\n";
-    final_r = Sort_the_table(R_table);
+    cout << "\n\n\n=================================================================================================================================================\n\n";
+    cout << "Assumption :\n\n";
+    cout << "1. Total Main Memory available in term of Frames is : " << M << "\n";
+    cout << "2. Each disc Block can fit into Frame of MM\n";
+
+    cout << "\n\tRelation R sorting started :\n";
+    string final_r = Sort_the_table(R_table);
     cout << "Relation R's sorted file path : " << final_r << "\n\n\n";
+
     cout << "\tRelation S sorting started :\n";
-    final_s = Sort_the_table(S_table);
-    cout << "Relation S's sorted file path : " << final_r << "\n\n ";
+    string final_s = Sort_the_table(S_table);
+    cout << "Relation S's sorted file path : " << final_s << "\n\n ";
     cout << "Total cost of sorting two Relation is : " << ALL_cost << "\n\n";
+
     cout << "Joining the tables : \n";
-    MergeJoin(final_r, final_s, M);
-    cout<<"Total cost of SortMergeJoin : "<<ALL_cost<<"\n";
-    cout<<"\n\n\n=================================================================================================================================================\n\n";
+    sortMergeJoin(final_r, final_s, M);
+    cout << "Total cost of SortMergeJoin : " << ALL_cost << "\n";
+    cout << "\n\n\n=================================================================================================================================================\n\n";
     return 0;
 }
